d1575eb1ac8c778d32e3e5ea27b0a6ec9c2343ab
Ia_grib <iagrib@bk.ru>
2018-02-18 21:44:54 +0200

    Fixed missing space

--- lua/staffchat/cl.lua
+++ lua/staffchat/cl.lua
@@ -5,7 +5,7 @@ local function printMsg()
 
 	if not IsValid(plr) then return end
 
-	chat.AddText(StaffChat.colors[2], "[", StaffChat.titles[room], "] ", StaffChat.colors[3], plr:Nick(), StaffChat.colors[4], " (", plr:SteamID(), ")", StaffChat.colors[2], ":", StaffChat.colors[StaffChat.read[room][plr:GetUserGroup()] and 5 or 6], msg)
+	chat.AddText(StaffChat.colors[2], "[", StaffChat.titles[room], "] ", StaffChat.colors[3], plr:Nick(), StaffChat.colors[4], " (", plr:SteamID(), ")", StaffChat.colors[2], ": ", StaffChat.colors[StaffChat.read[room][plr:GetUserGroup()] and 5 or 6], msg)
 end
 
 net.Receive("StaffChat", printMsg)
